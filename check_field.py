d7d7c00054b5f8b6b41c361fe6096f5e5dfe6825
Bo Li <liber1986@gmail.com>
2015-02-20 01:11:56 +0100

    comments and changes for week increment

--- check_field.py
+++ check_field.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python
+# -*- mode: python; c-basic-offset: 4 -*- vim: set sw=4 tw=70 et sta ai:
 
 import requests
 import bs4
 import weekdays
 import re
 import smtplib
+import sys
 from email.mime.text import MIMEText
 
-sat = weekdays.weekdays(5, 0).get_weekday_date()
-sun = weekdays.weekdays(6, 0).get_weekday_date()
+# offset for text debug
+offset = 1
 
+# week increment, 0 for current week
+shift = int(sys.argv[1])
+
+# get the date of Saturday and Sunday
+sat = weekdays.weekdays(5, shift).get_weekday_date()
+sun = weekdays.weekdays(6, shift).get_weekday_date()
+
+# FIXME: hard-coded text for url. Need to be updated the first time when
+# the source website updates
 root_url = 'http://kalender.soccarena-olympiapark.de/kalender'
 court_url = root_url + '/courts/courts.php?kalenderTag='
 
+# Yes so simple, this would be the url for field information of a given
+# date!
 sat_url = court_url + str(sat)
 sun_url = court_url + str(sun)
 
-
-offset = 0
-
 def get_frei_url (url):
+    # Get the http response from the url
     response = requests.get (url)
+    # use bs4 to parse the html text
     soup = bs4.BeautifulSoup (response.text)
+    # return the link for "bookable" court
     return [a.attrs.get('href') for a in soup.select('td.buchbar a[href^=../anfrage/anfrage.php]')]
 
 
@@ -36,12 +49,16 @@ def get_buch_url (url):
     return buch_url
 
 def send_buch_email (text):
+    '''
+    Send the text from the "sender" to "receiver" hard-coded here
+    FIXME: extremely insecure! DO NOT use any personal email as the sender
+    '''
 
     sender = 'bo.li.football.muc@gmail.com'
     receivers = ['tuhonghao@gmail.com', 'liber1986@gmail.com']
     # receivers = ['liber1986@gmail.com']
     username = 'bo.li.football.muc@gmail.com'
-    password = '1982528lB!'
+    password = 'm6w9kj874'
 
     # message = """From: From Bo Li <liber1986@gmail.com>
     # To: Team members
@@ -53,7 +70,7 @@ def send_buch_email (text):
     content = "Found the following place(s): \n" + text
 
     msg = MIMEText (content)
-    msg['Subject']= "Soccer field booking information test"
+    msg['Subject']= "Soccer field booking information for " + str(sat) + " and " + str(sun)
     msg['From'] = "Bo Li" + "<" + sender + ">"
     msg['To'] = ";".join(receivers)
 
@@ -68,16 +85,20 @@ def send_buch_email (text):
     except SMTPException:
 	print "Error: unable to send email"
 
+# main function starts
 if __name__ == "__main__":
 
+    # get the free url for Satday and Sunday
     frei_url_sat = get_frei_url(sat_url)
     frei_url_sun = get_frei_url(sun_url)
 
+    # Lazy work, combine the two url cells
     zusam_url = frei_url_sat + frei_url_sun
 
     text = ""
 
     for url_sat in zusam_url:
+	# put all the urls in an string array
         text = text +  get_buch_url (url_sat) + "\n"
 
     if offset:
