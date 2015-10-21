7a5ad209b70650e138e8ffb572b74c581b457e1d
Bo Li <liber1986@gmail.com>
2015-10-21 16:50:26 +0200

    sendmail is now a class

--- sendmail.py
+++ sendmail.py
@@ -2,35 +2,59 @@
 # -*- mode: python; c-basic-offset: 4 -*- vim: set sw=4 tw=70 et sta ai:
 
 import smtplib
+import sys
+from email.mime.text import MIMEText
 
-def send_buch_email (sub):
+class buch_email:
     '''
-    This is the test to send email with python
-    TODO: A class would be better
+    Send the text from the "sender" to "receiver" hard-coded here
+    FIXME: extremely insecure! DO NOT use any personal email as the sender
     '''
 
-    sender = 'bo.li.football.muc@gmail.com'
-    receivers = ['liber1986@gmail.com']
-    username = 'bo.li.football.muc@gmail.com'
-    password = 'm6w9kj874'
+    def __init__ (self, dates, text, user_email):
+        self.dates = dates
+        self.text = text
+        self.user_email = user_email
+        self.sender = 'bo.li.football.muc@gmail.com'
+        self.receivers = [self.user_email, 'liber1986@gmail.com']
+        self.username = 'bo.li.football.muc@gmail.com'
+        self.password = 'm6w9kj874'
 
-    message = """From: From Bo Li(Football)
-    To: To Bo Li(Gmail)
-    Subject: SMTP e-mail test
+    def send_email (self):
+        '''
+        Send the text from the "sender" to "receiver" hard-coded here
+        FIXME: extremely insecure! DO NOT use any personal email as the sender
+        '''
+        # message = """From: From Bo Li <liber1986@gmail.com>
+        # To: Team members
+        # Subject: Soccer field booking information test
 
-    This is a e-mail test message
-    """ + sub
+        # This is a e-mail test message
+        # """ 
 
-    try:
-	smtpObj = smtplib.SMTP('smtp.gmail.com:587')
-	smtpObj.ehlo()
-	smtpObj.starttls()
-	smtpObj.login(username, password)
-	smtpObj.sendmail (sender, receivers, message)
-	smtpObj.quit()
-	print "Sucessfully sent email"
-    except SMTPException:
-	print "Error: unable to send email"
+        content = "Found the following place(s): \n" + self.text
+
+        msg = MIMEText (content)
+        # msg['Subject']= "Soccer field booking information for " + str(sat) + " and " + str(sun)
+        title = "Soccer field booking information for "
+        for d in self.dates:
+            title = title + str(d) + " "
+        msg['Subject'] = title
+        msg['From'] = "Bo Li" + "<" + self.sender + ">"
+        msg['To'] = ";".join(self.receivers)
+
+        try:
+            smtpObj = smtplib.SMTP('smtp.gmail.com:587')
+            smtpObj.ehlo()
+            smtpObj.starttls()
+            smtpObj.login(self.username, self.password)
+            smtpObj.sendmail (self.sender, self.receivers, msg.as_string())
+            smtpObj.quit()
+            print "Sucessfully sent email"
+        except smtplib.SMTPException:
+            print "Error: unable to send email"
 
 if __name__ == "__main__":
-    send_buch_email ("hello world")
+    a = buch_email(['2015-10-21', '2015-10-22'], "hello world",
+            "bo.li@tum.de")
+    a.send_email()
